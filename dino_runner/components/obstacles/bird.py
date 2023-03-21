7a63a3904540a9d4b69571cf33dcea0b235829bb
Indigirl <119023866+indigirl@users.noreply.github.com>
2023-03-20 23:21:29 -0300

    Aula 3 - Menu, Score e death count

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -1,25 +1,24 @@
 import random
 
+from dino_runner.utils.constants import BIRD
 from dino_runner.components.obstacles.obstacle import Obstacle
 
 BIRD_HEIGHTS = [250, 290]
 
 
 class Bird(Obstacle):
-    def __init__(self, image):
-        self.type = 0              #o tipo é apenas um, animado
-        super().__init__(image, self.type)
+    def __init__(self):
+        super().__init__(BIRD, 0)
         self.rect.y = random.choice(BIRD_HEIGHTS)
         self.step_index = 0
 
-    def draw(self, screen):
-        if self.step_index >= 9:
-            self.step_index = 0
-        
+    def draw(self, screen):        
         screen.blit(self.image[self.step_index//5], self.rect)
         self.step_index += 1
 
- #caminho da imagem ObstacleManager> obstacle> bird
- #O Tipo será passado após a divisão, // pega apenas o primeiro numero inteiro, sem os decimais
+        if self.step_index >= 10:
+            self.step_index = 0
+
+ # // pega apenas o primeiro numero inteiro, sem os decimais
  #Até 5 o resultado da divisao é 0, a primeira imagem é passada = BIRD[0]
  #De 5 até 9 o resultado da divisão é 1, a segunda imagem é passada = BIRD [1]
--- dino_runner/components/obstacles/obstacleManager.py
+++ dino_runner/components/obstacles/obstacleManager.py
@@ -1,9 +1,7 @@
 import pygame
 import random
 
-from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
-from dino_runner.components.obstacles.cactusLarge import LargeCactus
-from dino_runner.components.obstacles.cactusSmall import SmallCactus
+from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.components.obstacles.bird import Bird
 
 
@@ -12,21 +10,22 @@ class ObstacleManager:
         self.obstacles = []
 
     def update(self, game):
-        if len(self.obstacles) == 0:
-            if random.randint(0, 2) == 0:
-                self.obstacles.append(SmallCactus(SMALL_CACTUS))
-            elif random.randint(0, 2) == 1:
-                self.obstacles.append(LargeCactus(LARGE_CACTUS))
-            elif random.randint(0, 2) == 2:
-                self.obstacles.append(Bird(BIRD))               
+        obstacle_type = [Cactus(), Bird()]
         
+        if len(self.obstacles) == 0:
+                self.obstacles.append(obstacle_type[random.randint(0,1)])
+                  
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
+                game.death_count += 1
                 break
 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+    def reset_obstacles(self):
+        self.obstacles = []
